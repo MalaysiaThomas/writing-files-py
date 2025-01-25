41724b78cacbe9a7dcbbf60cf0744eb9b500b64a
Malaysia Thomas <105211177+MalaysiaThomas@users.noreply.github.com>
2025-01-25 20:42:25 +0000

    add to csv

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import csv
 
-with open("cities.csv", "a") as file:
-    cities_file = csv.reader(file)
-
-    
+with open("cities.csv", "a") as cities_file:
+    cities_file.write("\nFlorence,23,cloudy\n")
+    cities_file.write("Dallas,48,sunny\n")
